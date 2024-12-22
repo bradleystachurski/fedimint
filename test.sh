76a4f1f4c96aed9efd1dbcd6a283b1980c9d807d
Brad Stachurski <bradley.stachurski@gmail.com>
2024-12-22 09:06:30 -0700

    add summary stats

--- test.sh
+++ test.sh
@@ -1,19 +1,9 @@
-#!/usr/bin/env bash
+for i in {1..10}; do
+  RUST_BACKTRACE=1 \
+  FM_DEVIMINT_DISABLE_MODULE_LNV2=1 \
+  just test-upgrades v0.4.0 current &>> results.log
+  echo "sleeping to give daemons time to shutdown" &>> results.log
+  sleep 10
+done
 
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.3 current &> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.4-rc.1 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.0 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.1 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.2 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.3 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.4.4 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 just test-upgrades v0.3.3 v0.4.0 current &>> results.log
-sleep 60
-FM_DEVIMINT_DISABLE_MODULE_LNV2=1 TEST_KINDS=fedimint-cli,gateway just test-upgrades v0.3.1 current &>> results.log
+
