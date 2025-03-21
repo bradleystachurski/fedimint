9236cc096da61386fa23f0d0038d5f4be0671d32
Brad Stachurski <bradley.stachurski@gmail.com>
2025-03-20 23:33:50 -0600

    wip: working locally, running manually vs docker 1

--- docker/deploy-fedimintd/deploy.sh
+++ docker/deploy-fedimintd/deploy.sh
@@ -14,64 +14,39 @@ ssh_host="${2:-${domain}}"
 # host dir we'll keep the files on
 host_dir=/root/fedimint-docker
 
-##
-## Verify intention
-##
-
-if [ -z "${FM_DOCKER_COMPOSE_DEPLOY:-}" ]; then
-  cat >&2 << EOF
-This script will use ssh to set up 'docker-compose'-based Fedimint node
-on $ssh_host which is supposed to be a fresh Ubuntu OS.
-
-To make sure you're aware of it, please set FM_DOCKER_COMPOSE_DEPLOY
-env variable.
-
-If unsure, please ask for help on the Fedimint Discord #mint-ops channel.
-EOF
-  exit 1
-fi
-
-
-if [ -z "${FM_DOCKER_COMPOSE_DEPLOY_WIPE:-}" ]; then
-  # shellcheck disable=SC2087
-  if ssh "root@$ssh_host" "test -e $host_dir" ; then
-
-    cat >&2 << EOF
-!!!
-!!! Previous installation of this script detected.
-!!!
-
-If your intention is to WIPE the previous installation
-and start from scratch, set FM_DOCKER_COMPOSE_DEPLOY_WIPE env var.
-EOF
-  exit 1
-
-  fi
-fi
-
-##
-## Wipe the previous host
-##
-
 echo >&2 '### Wiping previous setup'
 
 # shellcheck disable=SC2087
-ssh -q "root@$ssh_host" << EOF
-  touch ~/.hushlogin
-
-  systemctl stop fedimint-docker-compose 2>/dev/null || true
-
-  if [ -e $host_dir  ] ; then
-    # Stop the docker-compose stack
-    cd $host_dir && docker-compose down 2>/dev/null || true
-    # We remove only the fedimintd_data named volume
-    docker volume rm fedimint-docker_fedimintd_data 2>/dev/null || true
-  fi
-
+ssh -q "root@$ssh_host" << 'EOF'
+  systemctl stop fedimint-docker-compose
+  cd /root
   rm -rf /root/fedimint-docker
-  rm -rf /etc/systemd/system/fedimint-docker-compose.service
-  sudo systemctl daemon-reload
+  docker run --rm \
+    -v fedimint-docker_bitcoind_data:/data \
+    -v $(pwd):/backup \
+    busybox tar czvf /backup/volume_backup.tar.gz -C /data .
 
+  systemctl stop docker.socket
+  systemctl disable docker.socket
+  systemctl stop docker.service
+  systemctl disable docker.service
+
+  rm -rf /etc/systemd/system/fedimint-docker-compose.service
+  systemctl daemon-reload
+
+  apt-get purge -y $(dpkg -l | grep -Ei 'docker|containerd' | awk '{print $2}')
+  apt-get autoremove -y
+  rm -rf /var/lib/docker
+  rm -rf /var/lib/containerd
+
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh ./get-docker.sh
+  rm get-docker.sh
+
+  docker run --rm \
+    -v fedimint-docker_bitcoind_data:/data \
+    -v $(pwd):/backup \
+    busybox sh -c "cd /data && tar xzvf /backup/volume_backup.tar.gz"
 EOF
 
 
@@ -90,8 +65,8 @@ After=docker.service
 
 [Service]
 WorkingDirectory=${host_dir}
-ExecStart=/usr/bin/docker-compose up
-ExecStop=/usr/bin/docker-compose down
+ExecStart=/usr/bin/docker compose up
+ExecStop=/usr/bin/docker compose down
 Restart=always
 
 [Install]
@@ -108,51 +83,7 @@ ssh -q "root@$ssh_host" << EOF
 
   sed -i 's/my-super-host.com/$domain/g' $host_dir/.env
 
-  ufw allow 80/tcp
-  ufw allow 443/tcp
-  ufw allow 22/tcp
-  ufw --force enable
-
-  apt-get update && apt-get install -q -y docker-compose
-
   systemctl daemon-reload
   systemctl enable fedimint-docker-compose
   systemctl start fedimint-docker-compose
-EOF
-
-cat >&2 << EOF
-
-##
-## Done. Here are some things you might want to know:
-##
-
-Make sure you protect access to this system. AT MINIMUM,
-disable password-based authentication.
-See https://serverfault.com/a/909821 and similar articles.
-
-The firewall was configured and enabled for you.
-
-docker-compose services were defined in the $host_dir and
-if ever need arise you can adjust them from there. You can
-'cd' to that directory and use 'docker-compose' to do basic
-operations. E.g. 'docker-compose logs fedimintd' to view the
-logs, or 'docker-compose exec -u bitcoin bitcoin bash' to
-"enter" bitcoind container.
-
-Systemd unit 'fedimint-docker-compose' was installed and
-enabled to start all services automatically. Use
-
-* 'journalctl -u fedimint-docker-compose -f' to view all logs
-* 'systemctl status fedimint-docker-compose' to check status
-
-All service volumes are managed by docker/docker-compose and can
-be found in '/var/lib/docker/volumes/'.
-
-Bitcoin Core node will run in a pruned mode with minimum resource
-usage, but it might take it days to finish IBD. You can speed
-it up by copying an existing chain data.
-
-You should be able to go to https://$domain and access
-Fedimint Admin UI.
-
 EOF
