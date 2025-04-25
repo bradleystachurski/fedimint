e19ff99bd3951b57072352d653e78211161cbb48
Brad Stachurski <bradley.stachurski@gmail.com>
2025-04-24 18:11:25 -0600

    Revert "Merge pull request #7272 from dpc/dpc/jj-vlwlywqmmwur"
    This reverts commit f17a737974a1274f49aa024ec970e1df6a307812, reversing
    changes made to 36c37cd39b99c7e33bb75bf63bd9cf269204f723.

--- modules/fedimint-mint-client/src/lib.rs
+++ modules/fedimint-mint-client/src/lib.rs
@@ -346,10 +346,19 @@ impl FromStr for OOBNotes {
 }
 
 impl Display for OOBNotes {
+    /// Base64 encode a set of e-cash notes for out-of-band spending.
+    ///
+    /// Defaults to standard base64 for backwards compatibility.
+    /// For URL-safe base64 as alternative display use:
+    /// `format!("{:#}", oob_notes)`
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let bytes = Encodable::consensus_encode_to_vec(self);
 
-        f.write_str(&BASE64_URL_SAFE.encode(&bytes))
+        if f.alternate() {
+            f.write_str(&BASE64_URL_SAFE.encode(&bytes))
+        } else {
+            f.write_str(&base64::engine::general_purpose::STANDARD.encode(&bytes))
+        }
     }
 }
 
