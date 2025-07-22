0fc07eabd0f00b1b853766530a7946e386acc74a
Brad Stachurski <bradley.stachurski@gmail.com>
2025-07-22 08:03:54 -0600

    wip: debug

--- fedimint-startos/scripts/embassy.js
+++ fedimint-startos/scripts/embassy.js
@@ -1,19 +1,21 @@
 export const getConfig = async () => {
   return {
-    "bitcoin-rpc-user": {
-      "type": "string",
-      "name": "Bitcoin RPC Username",
-      "description": "Enter the RPC username from your Bitcoin Core config (found in Bitcoin Core > Config > RPC Settings > Username)",
-      "nullable": false,
-      "default": "bitcoin"
-    },
-    "bitcoin-rpc-password": {
-      "type": "string",
-      "name": "Bitcoin RPC Password",
-      "description": "Enter the RPC password from your Bitcoin Core config (found in Bitcoin Core > Config > RPC Settings > RPC Password)",
-      "nullable": false,
-      "default": "",
-      "masked": true
+    "spec": {
+      "rpcuser": {
+        "type": "string",
+        "name": "RPC Username",
+        "description": "Bitcoin Core RPC username",
+        "nullable": false,
+        "default": "bitcoin"
+      },
+      "rpcpass": {
+        "type": "string",
+        "name": "RPC Password",
+        "description": "Bitcoin Core RPC password",
+        "nullable": false,
+        "default": "",
+        "masked": true
+      }
     }
   };
 };
