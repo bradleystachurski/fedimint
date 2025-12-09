a4bcaa1eb17599bb9d5a489b10427c113b9d6a19
Brad Stachurski <bradley.stachurski@gmail.com>
2025-12-09 11:57:11 -0700

    wip: another approach

--- fedimint-startos/scripts/procedures/migrations.ts
+++ fedimint-startos/scripts/procedures/migrations.ts
@@ -1,4 +1,18 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-  .fromMapping({}, "0.9.1");
+  .fromMapping(
+    {
+      "0.9.0": {
+        up: compat.migrations.updateConfig(
+          (config: any) => config,  // Don't modify config
+          false,  // Return configured: false to skip validation
+          { version: "0.9.1", type: "up" }
+        ),
+        down: () => {
+          throw new Error("Cannot downgrade");
+        },
+      },
+    },
+    "0.9.1"
+  );
