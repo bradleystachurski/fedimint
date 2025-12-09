596f01a35bd59e099921219f99300113e593cafe
Brad Stachurski <bradley.stachurski@gmail.com>
2025-12-09 10:24:04 -0700

    wip: try migration script

--- fedimint-startos/scripts/procedures/migrations.ts
+++ fedimint-startos/scripts/procedures/migrations.ts
@@ -1,4 +1,25 @@
 import { compat, types as T } from "../deps.ts";
 
+const DEFAULT_RUST_LOG = "info,jsonrpsee_core::client::async_client=off,hyper=off,h2=off,jsonrpsee_server=warn,jsonrpsee_server::transport=off,AlephBFT-=error,iroh=error";
+
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "0.9.1" );
+  .fromMapping(
+    {
+      "0.9.0": {
+        up: compat.migrations.updateConfig(
+          (config: any) => {
+            config.advanced = {
+              "rust-log-level": DEFAULT_RUST_LOG
+            };
+            return config;
+          },
+          false,
+          { version: "0.9.1", type: "up" }
+        ),
+        down: () => {
+          throw new Error("Cannot downgrade");
+        },
+      },
+    },
+    "0.9.1"
+  );
